c1e23f8b7e8aa4a6010fc4eaf15b0023122c69d1
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-07 14:57:39 -0300

    feat: reducer and root

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import userReducer from './user/reducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ userReducer });
+
+export default rootReducer;
